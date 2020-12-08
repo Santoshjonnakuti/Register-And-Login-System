4eb122a60b71d39a6e651254bc5db4b180e7f766
Sid_Arth <53421109+sidharthkumar2019@users.noreply.github.com>
2020-12-08 20:15:43 +0530

    Add files via upload
    New_main contains a basic register and login system. Please test it on your systems and report any bugs. Also, share any other ideas.

--- New_main.py
+++ New_main.py
@@ -83,7 +83,51 @@ def register_user():
     password.delete(0,END)
 
 def login_user():
-    pass
+    user = username.get()
+    pasw = password.get()
+
+    if len(user)<5 or len(pasw)<5:
+        Label(screen2,text='len(inputs) should be >=5',fg='red').grid(row=5,column=1,columnspan=2)
+        return
+
+    # check whether a user with a given username exists
+    conn = sqlite3.connect('my_database.db')
+    c = conn.cursor()
+    c.execute('SELECT *,oid FROM UsernamePassword')
+    records = c.fetchall()  # OR .fetchone, .fetchmany(n)
+
+    user_exists = None
+    for record in records:
+        if record[0] == user:
+            #Label(screen1,text='__________User already exists___________',fg='red').grid(row=5,column=1)
+            user_exists = record
+            break
+
+    # if yes, then check whether the password is correct
+    if user_exists != None and pasw == record[1]:
+        screen3 = Toplevel()
+        screen3.title('Login_successful')
+        screen3.geometry('300x300')
+
+        frame_final = LabelFrame(screen3,padx=5,pady=5,bd=2)
+        frame_final.pack(padx=10,pady=10)
+
+        Label(frame_final,text='Welcome, '+user).pack()
+        Label(frame_final,text='').pack()
+        Label(frame_final,text='You have logged in successfully. Cheers!').pack()
+    elif user_exists == None:
+        # else display: 'No such user exists'
+        Label(screen2,text='__________No such user exists___________',fg='red').grid(row=5,column=1)
+    elif pasw!=record[1]:
+        # else display: 'wrong password'
+        Label(screen3,text='________Wrong password entered________',fg='red').grid(row=5,column=1)
+
+    conn.commit()
+    conn.close()
+
+    #clear the textboxes
+    username.delete(0,END)
+    password.delete(0,END)
 
 def register():
     global username
@@ -111,7 +155,29 @@ def register():
     Button(screen1,text='Go Back',command=screen1.destroy,width=20).grid(row=4,column=0,columnspan=2)
 
 def login():
-    pass
+    global username
+    global password
+    global screen2
+
+    screen2 = Toplevel()
+    screen2.title('Login_page')
+    screen2.geometry('300x300')
+
+    #create text boxes
+    username = Entry(screen2,width=30)
+    username.grid(row=1,column=1,padx=20)
+    password = Entry(screen2,width=30)
+    password.grid(row=2,column=1,padx=20)
+
+    #create textbox labels
+    Label(screen2,text='Enter your details to login:').grid(row=0,column=0,columnspan=2)
+    Label(screen2,text='Username').grid(row=1,column=0)
+    Label(screen2,text='Password').grid(row=2,column=0)
+
+    # create login button
+    Button(screen2,text='Login',command=login_user,width=20,).grid(row=3,column=0,columnspan=2)
+
+    Button(screen2,text='Go Back',command=screen2.destroy,width=20).grid(row=4,column=0,columnspan=2)
 
 frame = LabelFrame(root,padx=10,pady=10)
 frame.pack(padx=10,pady=10)
